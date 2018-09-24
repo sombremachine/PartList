a45faea7e6e0da9de30b78cd73912bff7289b1a5
darkness <no@mail.here>
2018-09-24 23:58:48 +0400

    simple error controller

--- src/main/java/test/controller/TestController.java
+++ src/main/java/test/controller/TestController.java
@@ -2,12 +2,12 @@ package test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import test.entity.ComputerComponent;
 import test.service.TestService;
@@ -83,6 +83,13 @@ public class TestController {
         modelAndView.setViewName("edit");
         return modelAndView;
     }
+
+//    @RequestMapping(value={"/error"}, method = RequestMethod.GET)
+//    public ModelAndView errorView() {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("error");
+//        return modelAndView;
+//    }
 
     @Transactional
     @RequestMapping(value={"/component/delete/{id}"}, method = RequestMethod.GET)
